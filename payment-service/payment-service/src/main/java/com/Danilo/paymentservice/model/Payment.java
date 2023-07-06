9997dea48cb76b02d77a6c32b60d08458aa99a10
Danilo <10500114@edp.com.br>
2023-07-06 00:13:43 -0300

    Adicionaando configuração de interceptação e tratamento de exceção. Alem disso foi inserido um objeto paymento

--- payment-service/payment-service/src/main/java/com/Danilo/paymentservice/model/Payment.java
+++ payment-service/payment-service/src/main/java/com/Danilo/paymentservice/model/Payment.java
@@ -1,2 +1,15 @@
-package com.Danilo.paymentservice.model;public class Payment {
+package com.Danilo.paymentservice.model;
+
+
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class Payment  implements Serializable {
+
+    private Long id;
+    private Long idUser;
+    private Long idProduct;
+    private String cardNumber;
 }
